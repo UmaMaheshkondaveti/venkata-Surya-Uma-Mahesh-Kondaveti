8feb0f69c0008526eb9a129ae9f6fea02d60b2bb
venkata surya uma mahesh kondaveti <124602557+UmaMaheshkondaveti@users.noreply.github.com>
2025-04-18 20:02:39 +0530

    Update Program-3.py

--- Program-3.py
+++ Program-3.py
@@ -1,23 +1,13 @@
 def generate_sequence(x):
-    # If x is even, reduce it by 1
-    if x % 2 == 0:
-        x = x - 1
-    
-   
     result = []
     current = 1
-    while current <= x:
+    count = 0
+    while count < x:
         result.append(current)
         current += 2
-    
+        count += 1
     return result
 
-
 num = int(input("Enter a number: "))
-
-
 seq = generate_sequence(num)
-if seq:
-    print(", ".join(map(str, seq)))
-else:
-    print("No sequence for this input")
+print(", ".join(map(str, seq)))
